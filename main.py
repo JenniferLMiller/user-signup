256eccaa2d59647d75d0b2a4f4702940fd961a58
Jennifer Miller <Jennifer@purplelime.com>
2017-01-30 20:16:17 -0600

    pulled validation functions to separate file

--- main.py
+++ main.py
@@ -16,11 +16,7 @@
 #
 import webapp2
 import cgi
-import re
-
-username_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-password_RE = re.compile(r"^.{3,20}$")
-email_RE = re.compile(r"^[\S]+[\S]+.[\S]+$")
+from validation_helpers import valid_username, valid_password, valid_email
 
 # html boilerplate for the top of every page
 page_header = """
@@ -31,6 +27,7 @@ page_header = """
         <style type="text/css">
             .error {
                 color: red;
+                font-size: larger;
                 }
         </style>
     </head>
@@ -45,14 +42,11 @@ page_footer = """
     </body>
     </html>
     """
-def valid_username(username):
-    return username_RE.match(username)
 
-def valid_password(password):
-    return password_RE.match(password)
-
-def valid_email(email):
-    return email_RE.match(email)
+error_bad_username = ""
+error_bad_password = ""
+error_password_mismatch = ""
+error_bad_email = ""
 
 class Index(webapp2.RequestHandler):
     """
@@ -61,22 +55,31 @@ class Index(webapp2.RequestHandler):
     def get(self):
         page_header = "<h2>Create an account:</h2>"
 
-        account_form = """
+        user_form = """
         <form action="/signup" method="post">
         <label>
             Username:
             <input type="text" name="username" />
         </label>
+        """
+
+        pass_form = """
         <br>
         <label>
             Password:
             <input type="password" name="password" />
         </label>
+        """
+
+        verify_pass_form = """
         <br>
         <label>
             Verify Password:
             <input type="password" name="verify_password" />
         </label>
+        """
+
+        email_form = """
         <br>
         <label>
             Email (optional):
@@ -85,7 +88,15 @@ class Index(webapp2.RequestHandler):
         <br>
         <input type="submit" value="Sign me up!"/>
         """
-        content = page_header + account_form + page_footer
+        account_form = user_form + pass_form + verify_pass_form + email_form
+
+        user_content = page_header + user_form + error_bad_username
+        pass_content = pass_form + error_bad_password
+        verify_pass_content = verify_pass_form + error_password_mismatch
+        email_content = email_form + error_bad_email + page_footer
+
+        content = user_content + pass_content + verify_pass_content + email_content
+
         self.response.write(content)
 
 class Signup(webapp2.RequestHandler):
@@ -93,29 +104,74 @@ class Signup(webapp2.RequestHandler):
        Handles requests coming to "/signup", validates data
     """
     def post(self):
-        #"""
-        #   Retrieve data entered, validate and sanitize
-        #"""
+        """
+           Retrieve data entered, validate and sanitize
+        """
         username_in = self.request.get("username")
         password_in = self.request.get("password")
         verify_password_in = self.request.get("verify_password")
         email_in = self.request.get("email")
 
-        #good_username = valid_username(username_in)
-        #good_password = valid_password(password_in)
-        #good_email = valid_email(email_in)
+        # validate username
+        good_username = valid_username(username_in)
+
+        if good_username:
+            #sanitize username
+            ok_username = cgi.escape(username_in, quote=True)
+            error_bad_username = ""
+        else:
+            error_bad_username = "<span class='error'>Invalid username</span>"
+
+        #validate password and compare against verify_password_in
+        good_password = valid_password(password_in)
+
+        if good_password:
+            #make sure the passwords match
+            error_bad_password = ""
+            if password_in == verify_password_in:
+                #sanitize password
+                ok_password = cgi.escape(password_in,quote=True)
+                good_match = True
+                error_password_mismatch = ""
+            else:
+                good_match = False
+                error_password_mismatch = """
+                "<span class='error'>Passwords did not match</span>"
+                """
+        else:
+            error_bad_password = "<span class='error'>Invalid password</span>"
+
+        #validate email address
+        good_email = valid_email(email_in)
+
+        if good_email:
+            #sanitize email
+            ok_email = cgi.escape(email_in, quote=True)
+            error_bad_email = ""
+        else:
+            error_bad_email = "<span class='error'>Invalid username</span>"
+
+        if good_username and good_password and good_match and good_email:
+            welcome_text = "<h1>Welcome, " + ok_username + "!</h1>"
+            self.response.write(welcome_text)
+        else:
+            self.redirect("/")
+
 
 class WelcomePage(webapp2.RequestHandler):
     """
        Handles requests coming to "/welcome", builds Welcome page
     """
     def get(self):
-        username = "Jen"   ## temporary!
-        welcome_text = "<h1>Welcome, " + username + "!</h1>"
+        welcome_text = "<h1>Welcome, " + ok_username + "!</h1>"
         self.response.write(welcome_text)
+
+#app = webapp2.WSGIApplication([
+#    ('/', Index),
+#    ('/signup', Signup),
+#    ('/welcome', WelcomePage)
 
 app = webapp2.WSGIApplication([
     ('/', Index),
-    ('/signup', Signup),
-    ('/welcome', WelcomePage)
+    ('/signup', Signup)
 ], debug=True)
---
+++ validation_helpers.py
@@ -0,0 +1,22 @@
+import re
+
+def valid_username(username):
+    username_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    checkname = username_RE.match(username)
+    if checkname is None:
+        return False
+    return True
+
+def valid_password(password):
+    password_RE = re.compile(r"^.{3,20}$")
+    checkpwd = password_RE.match(password)
+    if checkpwd is None:
+        return False
+    return True
+
+def valid_email(email):
+    email_RE = re.compile(r"^[\S]+[\S]+.[\S]+$")
+    checkemail = email_RE.match(email)
+    if checkemail is None:
+        return False
+    return True
